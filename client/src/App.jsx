0ab981772ced85b0645a066404d4eca9de613a4a
Seydur <seydur.rahman82@gmail.com>
2025-04-21 20:12:00 +0600

    first comments

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" text-red-500">
+      <p className="text-red-500">Hello world!</p>
+      <h1 className="text-green-500">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
